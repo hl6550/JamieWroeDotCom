98d7adf8c89a371539c8553a81004820354a386f
Jamie <JanJansen2634@gmail.com>
2013-07-06 21:37:18 +0100

    Specify view names in controller for testing

--- source/JamieWroeDotCom.UI/Controllers/HomeController.cs
+++ source/JamieWroeDotCom.UI/Controllers/HomeController.cs
@@ -7,21 +7,22 @@
 
         public ActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return View("About");
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View("Contact");
+        }
 
-            return View();
+        public ActionResult Portfolio()
+        {
+            return View("Portfolio");
         }
 
     }
