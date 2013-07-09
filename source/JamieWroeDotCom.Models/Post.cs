b656d45d33da6e8752dd3aa6f5ee34b8f9d00019
Jamie <JanJansen2634@gmail.com>
2013-07-09 19:28:55 +0100

    Added new Models project
    With post and user .cs files

---
+++ source/JamieWroeDotCom.Models/Post.cs
@@ -0,0 +1,30 @@
+﻿namespace JamieWroeDotCom.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Post
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public string ThumbnailGuid { get; set; }
+        public string Blurb { get; set; }
+        public IEnumerable<String> Keywords { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+
+    }
+
+    public enum Status
+    {
+
+        Draft,
+        Published,
+        Deleted
+
+    }
+}
---
+++ source/JamieWroeDotCom.Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamieWroeDotCom.Models
+{
+    class User
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UsertName { get; set; }
+    }
+}
