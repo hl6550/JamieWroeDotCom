f4fa295f1c50219c73934cbb59e88d0455527d8f
Jamie <JanJansen2634@gmail.com>
2013-07-11 22:06:53 +0100

    Added new entities to map simple authentication tables

---
+++ source/JamieWroeDotCom.Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace JamieWroeDotCom.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public ICollection<User> Users { get; set; }
+
+        public Role()
+        {
+            Users = new List<User>();
+        }
+    }
+}
--- source/JamieWroeDotCom.Models/User.cs
+++ source/JamieWroeDotCom.Models/User.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace JamieWroeDotCom.Models
 {
     public class User
     {
-
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
+        public ICollection<Role> Roles { get; set; }
+
+        public User()
+        {
+            Roles = new List<Role>();
+        }
     }
 }
