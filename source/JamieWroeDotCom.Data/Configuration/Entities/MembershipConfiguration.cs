dafc4c5dca1bc82be29a51d29ffa1ef6b717318e
Jamie <JanJansen2634@gmail.com>
2013-07-11 22:15:12 +0100

    Add more config files for new membership tables

---
+++ source/JamieWroeDotCom.Data/Configuration/Entities/MembershipConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using JamieWroeDotCom.Models;
+
+namespace JamieWroeDotCom.Data.Configuration.Entities
+{
+    internal class MembershipConfiguration : EntityTypeConfiguration<Membership>
+    {
+        public MembershipConfiguration()
+        {
+            ToTable("webpages_Membership");
+            HasKey(p => p.UserId);
+
+            Property(p => p.UserId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+
+            Property(p => p.ConfirmationToken).HasMaxLength(128)
+                                              .HasColumnType("nvarchar");
+
+            Property(p => p.PasswordFailuresSinceLastSuccess).IsRequired();
+
+            Property(p => p.Password).IsRequired()
+                                     .HasMaxLength(128)
+                                     .HasColumnType("nvarchar");
+
+            Property(p => p.PasswordSalt).IsRequired()
+                                         .HasColumnType("nvarchar")
+                                         .HasMaxLength(128);
+
+            Property(p => p.PasswordVerificationToken).HasMaxLength(128)
+                                                      .HasColumnType("nvarchar");
+        }
+    }
+}
---
+++ source/JamieWroeDotCom.Data/Configuration/Entities/OAuthMembershipConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using JamieWroeDotCom.Models;
+
+namespace JamieWroeDotCom.Data.Configuration.Entities
+{
+    public class OAuthMembershipConfiguration : EntityTypeConfiguration<OAuthMembership>
+    {
+        public OAuthMembershipConfiguration()
+        {
+            ToTable("webpages_OAuthMembership");
+
+            HasKey(k => new { k.Provider, k.ProviderUserId });
+
+            Property(p => p.Provider).HasMaxLength(30)
+                                     .IsRequired()
+                                     .HasColumnType("nvarchar");
+
+            Property(p => p.ProviderUserId).HasMaxLength(100)
+                                           .IsRequired()
+                                           .HasColumnType("nvarchar");
+
+            Property(p => p.UserId).IsRequired();
+        }
+    }
+}
